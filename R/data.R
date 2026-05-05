883849056e34cc58a14c015cdd9412f9ff9acdda
sho-125 <sanghyun.hong@canterbury.ac.nz>
2026-05-05 15:06:54 +1200

    Submit qgarch 0.1.0 to CRAN

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Monthly U.S. data
+#'
+#' Monthly data used in examples and tests for the qgarch package.
+#'
+#' @format A data frame with monthly observations and 3 variables:
+#' \describe{
+#'   \item{YearMon}{Month of the observation.}
+#'   \item{Mktrf}{Market excess return.}
+#'   \item{ER}{Excess return series.}
+#' }
+#'
+#' @source Prepared by the package author.
+"us_monthly"
